0d203076959ea1a4bc91ec12ecd0895c2e166634
chengdonghang@szfalcon.com <381322162@qq.com>
2018-08-20 12:06:58 +0800

    简书header1.0

---
+++ src/common/header/store/actionCreators.js
@@ -0,0 +1,9 @@
+import { contants } from "./index";
+
+export const searchFocus = () => ({
+    type: contants.SEARCH_FOCUS
+});
+
+export const searchBlur = () => ({
+    type: contants.SEARCH_BLUR
+});

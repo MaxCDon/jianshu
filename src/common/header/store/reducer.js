ada89d6856b0a3df07833a713b8f225b45bae436
chengdonghang@szfalcon.com <381322162@qq.com>
2018-08-20 17:17:10 +0800

    使用immutable

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,19 @@
 import { contants } from "./index";
+import { fromJS } from 'immutable';
 
-const defaultState = {
+//immutable库
+//immutable对象
+
+const defaultState = fromJS({
     focused: false,
-};
+});
 
 export default (state = defaultState, action) => {
     if(action.type === contants.SEARCH_FOCUS){
-        const newState = JSON.parse(JSON.stringify(state));
-        newState.focused = true;
-        return newState;
+        return state.set('focused', true);
     }
     if(action.type === contants.SEARCH_BLUR){
-        const newState = JSON.parse(JSON.stringify(state));
-        newState.focused = false;
-        return newState;
+        return state.set('focused', false);
     }
 
     return state;

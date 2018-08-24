95f345f6a5f0d10112033d8ed9a0f3b7304673f5
chengdonghang@szfalcon.com <381322162@qq.com>
2018-08-24 16:46:32 +0800

    动态路由配置

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { actionCreators }from './store'
+import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component{
     render(){
+        const {title, content} = this.props;
         return(
-            <div>detail</div>
+            <DetailWrapper>
+                <Header>{title}</Header>
+                <Content dangerouslySetInnerHTML={{__html: content}} />
+            </DetailWrapper>
         )
+    }
+
+    componentDidMount(){
+        this.props.getDetailData(this.props.match.params.id);
     }
 }
 
-export default Detail;
+const mapState = (state) => ({
+    title: state.getIn(['detail','title']),
+    content: state.getIn(['detail','content']),
+});
+
+const mapDispatch = (dispatch) => ({
+    getDetailData(id){
+        const action = actionCreators.getDetail(id);
+        dispatch(action)
+    }
+});
+
+export default connect(mapState,mapDispatch)(Detail);
